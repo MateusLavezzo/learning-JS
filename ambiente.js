967a4d53b0b90c90fe4ffa1aad545e893865fe6d
Mateus Lavezzo <145409374+MateusLavezzo@users.noreply.github.com>
2024-01-16 21:37:49 -0300

    Add files via upload

---
+++ ambiente.js
@@ -0,0 +1,28 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+} while(c <= 6)
+
+
+
+
+
+
+
+var x = 1
+while(x <= 500) {
+    console.log(`Passo ${c}`)
+    x++
+}
+
+
+
+
+
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')             
